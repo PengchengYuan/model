4f2aaa2098988cf14e0dd2aa8bd7a0f386e84bfb
PengchengYuan <yuan.peng.cheng@163.com>
2017-03-01 16:39:15 +0800

    拖拽

--- js/bundle.js
+++ js/bundle.js
@@ -42,19 +42,27 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports, __webpack_require__) {
-
-	console.log('9877');
-	var ab = __webpack_require__(1);
-	console.log(ab.a());
-
-/***/ },
-/* 1 */
 /***/ function(module, exports) {
 
-	module.exports = {a:function(){
-		console.log('8aa88');	 	
-	}};
+	function Drag(dom){
+		dom.onmousedown = function(event){
+			var event = window.event || event;
+			var x1 = event.offsetX;
+			var y1 = event.offsetY;
+			document.onmousemove = function(event){
+				var event = window.event || event;
+				var x = event.clientX;
+				var y = event.clientY;
+				dom.style.left = x -x1 + 'px';
+				dom.style.top = y -y1 + 'px';
+			};
+			dom.onmouseup = function(){
+				document.onmousemove = null;
+			};
+		};	 	
+	}
+	var dom = document.getElementById('box');
+	Drag(dom);
 
 /***/ }
 /******/ ]);
--- js/entry/entry.js
+++ js/entry/entry.js
@@ -1,3 +1,19 @@
-console.log('9877');
-var ab = require('../dfsef/xgdsg.js');
-ab.a();
+function Drag(dom){
+	dom.onmousedown = function(event){
+		var event = window.event || event;
+		var x1 = event.offsetX;
+		var y1 = event.offsetY;
+		document.onmousemove = function(event){
+			var event = window.event || event;
+			var x = event.clientX;
+			var y = event.clientY;
+			dom.style.left = x -x1 + 'px';
+			dom.style.top = y -y1 + 'px';
+		};
+		dom.onmouseup = function(){
+			document.onmousemove = null;
+		};
+	};	 	
+}
+var dom = document.getElementById('box');
+Drag(dom);
